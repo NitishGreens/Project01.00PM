618cf3652b37dc034290afcad599bbd53f35c99f
NitishGreens <nitishgreens@gmail.com>
2021-10-28 19:55:03 +0530

    updated jos

--- src/org/hexa/Employee.java
+++ src/org/hexa/Employee.java
@@ -1,6 +1,20 @@
 package org.hexa;
 
 public class Employee {
+	private void sub() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void sub1() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void sub2() {
+		// TODO Auto-generated method stub
+
+	}
 
 	public static void main(String[] args) {
 
